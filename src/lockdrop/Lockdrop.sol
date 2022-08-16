8d440a6aab1c57adce28da07c0c5ed254fb54607
bkioshn <bkioshn@ip-192-168-2-45.ap-southeast-1.compute.internal>
2022-08-16 17:58:27 +0700

    Remove LockdropConfig and turns to single var, add modifier for deposit period, add out of deposit period test case, format the code

--- src/lockdrop/Lockdrop.sol
+++ src/lockdrop/Lockdrop.sol
@@ -8,71 +8,86 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Lockdrop is ReentrancyGuard, Ownable {
-    
-    // --- Libraries ---
-    using SafeERC20 for IERC20;
+  // --- Libraries ---
+  using SafeERC20 for IERC20;
 
-    // --- Events ---
-    event LogLockToken(address indexed account, address _token, uint256 _amount, uint256 _lockPeriod);
+  // --- Events ---
+  event LogLockToken(
+    address indexed account,
+    address token,
+    uint256 amount,
+    uint256 lockPeriod
+  );
 
-    // --- Custom Errors ---
-    error LockDrop_ZeroAddressNotAllowed();
-    error LockDrop_InvalidStartLockTimestamp();
-    error LockDrop_InvalidLockPeriod();
-    error LockDrop_MismatchToken();
+  // --- Custom Errors ---
+  error LockDrop_ZeroAddressNotAllowed();
+  error LockDrop_InvalidStartLockTimestamp();
+  error LockDrop_InvalidLockPeriod();
+  error LockDrop_MismatchToken();
+  error LockDrop_NotInDepositPeriod();
 
-    // --- Structs ---
-    struct LockdropConfig {
-        IERC20 lockdropToken; // lockdrop token address
-        uint256 startLockTimestamp; // timestamp for starting lockdrop event
-        uint256 endLockTimestamp; // timestamp for deposit period after start lockdrop event
-        uint256 withdrawalTimestamp; // timestamp for withdraw period after start lockdrop event
-    }
+  // --- Structs ---
 
-    struct LockdropState {
-        uint256 lockdropTokenAmount;
-        uint256 lockPeriod;
-    }
+  struct LockdropState {
+    uint256 lockdropTokenAmount;
+    uint256 lockPeriod;
+  }
 
-    // --- States ---
-    LockdropConfig public lockdropConfig;
-    mapping(address => LockdropState) public LockdropStates;
+  // --- States ---
+  IERC20 public lockdropToken; // lockdrop token address
+  uint256 public startLockTimestamp; // timestamp for starting lockdrop event
+  uint256 public endLockTimestamp; // timestamp for deposit period after start lockdrop event
+  uint256 public withdrawalTimestamp; // timestamp for withdraw period after start lockdrop event
 
+  mapping(address => LockdropState) public LockdropStates;
 
-    constructor(address _lockdropToken, uint256 _startLockTimestamp) {
-        if (_lockdropToken == address(0)) revert LockDrop_ZeroAddressNotAllowed();
-        if (block.timestamp > _startLockTimestamp) revert LockDrop_InvalidStartLockTimestamp();
+  // --- Modifiers ---
+  /// @dev Only able to proceed during deposit period
+  modifier onlyInDepositPeriod() {
+    if (
+      block.timestamp < startLockTimestamp ||
+      block.timestamp > withdrawalTimestamp
+    ) revert LockDrop_NotInDepositPeriod();
+    _;
+  }
 
-        lockdropConfig = LockdropConfig(
-             IERC20(_lockdropToken),
-             _startLockTimestamp,
-             _startLockTimestamp + 604800, // 7 days in second
-             _startLockTimestamp + 432000 // 5 days in second
-        );
-    }
+  constructor(address _lockdropToken, uint256 _startLockTimestamp) {
+    if (_lockdropToken == address(0)) revert LockDrop_ZeroAddressNotAllowed();
+    if (block.timestamp > _startLockTimestamp)
+      revert LockDrop_InvalidStartLockTimestamp();
 
-    /// @dev User lock ERC20 Token
-    /// @param _token Token address that user wants to lock
-    /// @param _amount Number of token that user wants to lock
-    /// @param _lockPeriod Number of second that user wants to lock 
-    function lockToken(address _token, uint256 _amount, uint256 _lockPeriod)external {
-        if (_amount == 0) revert LockDrop_ZeroAddressNotAllowed();
-        if (_lockPeriod < 604800) revert LockDrop_InvalidLockPeriod(); // Less than 1 week
-        if (_lockPeriod > 31449600) revert LockDrop_InvalidLockPeriod(); // More than 52 weeks
-        if (_token != address(lockdropConfig.lockdropToken)) revert LockDrop_MismatchToken(); // Mismatch token address
+    lockdropToken = IERC20(_lockdropToken);
+    startLockTimestamp = _startLockTimestamp;
+    endLockTimestamp = _startLockTimestamp + 604800; // 7 days in second
+    withdrawalTimestamp = _startLockTimestamp + 432000; // 5 days in second
+  }
 
-        IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
-        LockdropStates[msg.sender] = LockdropState({lockdropTokenAmount: _amount, lockPeriod: _lockPeriod});
+  /// @dev User lock ERC20 Token
+  /// @param _token Token address that user wants to lock
+  /// @param _amount Number of token that user wants to lock
+  /// @param _lockPeriod Number of second that user wants to lock
+  function lockToken(
+    address _token,
+    uint256 _amount,
+    uint256 _lockPeriod
+  ) external onlyInDepositPeriod {
+    if (_amount == 0) revert LockDrop_ZeroAddressNotAllowed();
+    if (_lockPeriod < 604800) revert LockDrop_InvalidLockPeriod(); // Less than 1 week
+    if (_lockPeriod > 31449600) revert LockDrop_InvalidLockPeriod(); // More than 52 weeks
+    if (_token != address(lockdropToken)) revert LockDrop_MismatchToken(); // Mismatch token address
 
-        emit LogLockToken(msg.sender, _token, _amount, _lockPeriod);
-    }
+    IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
+    LockdropStates[msg.sender] = LockdropState({
+      lockdropTokenAmount: _amount,
+      lockPeriod: _lockPeriod
+    });
 
-    function mintPLP() internal {
-        // get number of PLP that should be mint
-    }
+    emit LogLockToken(msg.sender, _token, _amount, _lockPeriod);
+  }
 
-    function stakePLP() internal {
+  function mintPLP() internal {
+    // get number of PLP that should be mint
+  }
 
-    }
-
+  function stakePLP() internal {}
 }
--- src/tests/lockdrop/LockdropTest.t.sol
+++ src/tests/lockdrop/LockdropTest.t.sol
@@ -6,35 +6,58 @@ import "../base/DSTest.sol";
 import { console } from "../utils/console.sol";
 import { BaseTest } from "../base/BaseTest.sol";
 import { Lockdrop } from "../../lockdrop/Lockdrop.sol";
-import { MockERC20 } from "../mock/MockERC20.sol";
+import { MockErc20 } from "../mocks/MockERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+error LockDrop_NotInDepositPeriod();
+
 contract LockdropTest is BaseTest {
-    Lockdrop internal lockdrop;
-    MockERC20 internal mockERC20;
+  Lockdrop internal lockdrop;
+  MockErc20 internal mockERC20;
 
-    function setUp() external {
-        mockERC20 = new MockERC20("Mock Token", "MT");
-        lockdrop = new Lockdrop(address(mockERC20), 100000);
-    }
+  function setUp() external {
+    mockERC20 = new MockErc20("Mock Token", "MT", 18);
+    lockdrop = new Lockdrop(address(mockERC20), 100000);
+  }
 
-    function test_LockdropIsInit_ShouldBeCorrectlyInit() external {
-        (IERC20 lockdropToken, uint startLockTimestamp, uint endLockTimestamp, uint withdrawalTimestamp)= lockdrop.lockdropConfig();
-        assertEq(address(lockdropToken), address(mockERC20));
-        assertEq(startLockTimestamp, 100000);
-        assertEq(endLockTimestamp, 704800);
-        assertEq(withdrawalTimestamp, 532000);
-    }
+  function test_LockdropIsInit_ShouldBeCorrectlyInit() external {
+    assertEq(lockdrop.lockdropToken.address, address(mockERC20));
+    assertEq(lockdrop.startLockTimestamp, 100000);
+    assertEq(lockdrop.endLockTimestamp, 704800);
+    assertEq(lockdrop.withdrawalTimestamp, 532000);
+  }
 
-    function test_LockdropLockToken_ShouldWorkCorrectly() external {
-        (IERC20 lockdropToken, uint startLockTimestamp, uint endLockTimestamp, uint withdrawalTimestamp)= lockdrop.lockdropConfig();
-        vm.startPrank(ALICE, ALICE);
-        mockERC20.mint(ALICE, 20);
-        mockERC20.approve(address(lockdrop), 20);
-        lockdrop.lockToken(address(mockERC20), 16, 604900);
-        vm.stopPrank();
-        (uint256 lockdropTokenAmount, uint256 lockPeriod) = lockdrop.LockdropStates(ALICE);
-        assertEq(lockdropTokenAmount, 16);
-        assertEq(lockPeriod, 604900);
-    }
+  function test_LockdropLockToken_ShouldWorkCorrectly() external {
+    vm.startPrank(ALICE, ALICE);
+    mockERC20.mint(ALICE, 20);
+    mockERC20.approve(address(lockdrop), 20);
+    vm.warp(120000);
+    lockdrop.lockToken(address(mockERC20), 16, 604900);
+    vm.stopPrank();
+    (uint256 lockdropTokenAmount, uint256 lockPeriod) = lockdrop.LockdropStates(
+      ALICE
+    );
+    assertEq(lockdropTokenAmount, 16);
+    assertEq(lockPeriod, 604900);
+  }
+
+  function test_LockdropLockToken_InWithdrawPeriod() external {
+    vm.startPrank(ALICE, ALICE);
+    mockERC20.mint(ALICE, 20);
+    mockERC20.approve(address(lockdrop), 20);
+    vm.warp(532500);
+    vm.expectRevert(LockDrop_NotInDepositPeriod.selector);
+    lockdrop.lockToken(address(mockERC20), 16, 604900);
+    vm.stopPrank();
+  }
+
+  function test_LockdropLockToken_ExceedDepositAndWithdrawPeriod() external {
+    vm.startPrank(ALICE, ALICE);
+    mockERC20.mint(ALICE, 20);
+    mockERC20.approve(address(lockdrop), 20);
+    vm.warp(535000);
+    vm.expectRevert(LockDrop_NotInDepositPeriod.selector);
+    lockdrop.lockToken(address(mockERC20), 16, 604900);
+    vm.stopPrank();
+  }
 }
