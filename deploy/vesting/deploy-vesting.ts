ae15f91f823d5980229fa8f7b7b2ab97307d5521
Jonathan Xina <john.xina.p88@gmail.com>
2022-08-18 17:15:23 +0700

    feat: deployment script for Vester

---
+++ deploy/vesting/deploy-vesting.ts
@@ -0,0 +1,24 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
+
+const ESP88 = "0xB0897464c0b0C400052fD292Db28A2942df1e705";
+const P88 = "0xB0897464c0b0C400052fD292Db28A2942df1e705";
+const VESTED_ADDRESS = "0x0000000000000000000000000000000000000000";
+const UNUSED_ADDRESS = "0x000000000000000000000000000000000000dead";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const deployer = (await ethers.getSigners())[0];
+  const Vester = await ethers.getContractFactory("Vester", deployer);
+  const vester = await Vester.deploy(
+    ESP88,
+    P88,
+    VESTED_ADDRESS,
+    UNUSED_ADDRESS
+  );
+  console.log(`Deploying Vester Contract`);
+  console.log(`Deployed at: ${vester.address}`);
+};
+
+export default func;
+func.tags = ["Vester"];
