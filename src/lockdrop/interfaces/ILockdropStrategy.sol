c95c659f9e97caf1743a9e6aa117f68f9d02de90
XingChing <c7243359@gmail.com>
2022-08-26 11:31:33 +0700

    fix: merge simple strategy logic to lockdrop staking PLP

--- src/lockdrop/interfaces/ILockdropStrategy.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.14;
-
-interface ILockdropStrategy {
-  function execute(uint256 tokenAmount, address tokenAddress)
-    external
-    returns (uint256);
-}
