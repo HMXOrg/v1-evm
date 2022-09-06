ac89b00b5ece55cd5a8105d03e0dba0f562e2534
Jonathan Xina <john.xina.p88@gmail.com>
2022-09-06 11:14:44 +0700

    increase position

--- deploy/core/set-router.ts
+++ deploy/core/set-router.ts
@@ -4,7 +4,7 @@ import { ethers } from "hardhat";
 import { PoolConfig__factory } from "../../typechain";
 
 const CONFIG = "0xD941773cfaC0EaE3e2d790EaB7cCfADe0Ab87e23";
-const PoolRouter = "0xC285a535403bfa849cdDA96EE85bc39D470f3416";
+const PoolRouter = "0x3760f978AcE668209E415c0576a4d4f064850226";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const deployer = (await ethers.getSigners())[0];
---
+++ deploy/test/increase-position.ts
@@ -0,0 +1,48 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
+import {
+  ERC20__factory,
+  MintableTokenInterface__factory,
+  PoolRouter__factory,
+} from "../../typechain";
+import config from "../../contracts.mumbai.json";
+
+const POOL_ROUTER = config.PoolRouter;
+const COLLATERAL_TOKEN = config.Tokens.WBTC;
+const POOL = config.Pools[0].address;
+
+enum Exposure {
+  LONG,
+  SHORT,
+}
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const deployer = (await ethers.getSigners())[0];
+  const poolRouter = PoolRouter__factory.connect(POOL_ROUTER, deployer);
+  const collateralToken = ERC20__factory.connect(COLLATERAL_TOKEN, deployer);
+
+  // await (
+  //   await collateralToken.approve(
+  //     poolRouter.address,
+  //     ethers.constants.MaxUint256
+  //   )
+  // ).wait();
+
+  await (
+    await poolRouter.increasePosition(
+      POOL,
+      0,
+      COLLATERAL_TOKEN,
+      ethers.utils.parseUnits("1", 8),
+      COLLATERAL_TOKEN,
+      ethers.utils.parseUnits("50000", 30),
+      Exposure.LONG,
+      { gasLimit: 10000000 }
+    )
+  ).wait();
+  console.log(`Execute increasePosition`);
+};
+
+export default func;
+func.tags = ["IncreasePosition"];
