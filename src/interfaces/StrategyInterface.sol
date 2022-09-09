d772be9ccf94333fa1cc105f6db1b8538386f9c6
shutsuwei <shu.tsu.wei88@gmail.com>
2022-09-09 11:26:04 +0700

    chore: update solidity version

--- src/interfaces/StrategyInterface.sol
+++ src/interfaces/StrategyInterface.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.14;
+pragma solidity 0.8.16;
 
 interface StrategyInterface {
   /// @notice Send the tokens to the strategy and call run to perform the actual strategy logic
--- src/tests/core/pool-diamond/PoolDiamond_SetStrategyOf.t.sol
+++ src/tests/core/pool-diamond/PoolDiamond_SetStrategyOf.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.14;
+pragma solidity 0.8.16;
 
 import { PoolDiamond_BaseTest, LibPoolConfigV1, MockDonateVault, MockStrategy, Pool, console, GetterFacetInterface, LiquidityFacetInterface } from "./PoolDiamond_BaseTest.t.sol";
 import { StrategyInterface } from "../../../interfaces/StrategyInterface.sol";
