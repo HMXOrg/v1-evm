6097b2be9bd1dadfa77574adcf950e03436ca18e
sgperp88 <sg.perp88@gmail.com>
2022-09-20 18:57:33 +0700

    added view

--- src/interfaces/ICurvePoolV3.sol
+++ src/interfaces/ICurvePoolV3.sol
@@ -2,5 +2,5 @@
 pragma solidity 0.8.17;
 
 interface ICurvePoolV3 {
-  function balances(uint256 tokenIndex) external returns (uint256);
+  function balances(uint256 tokenIndex) external view returns (uint256);
 }
