dc7bb4555a863a6cb73ce41c2d716b7516bf85b7
Jonathan Xina <john.xina.p88@gmail.com>
2022-12-07 17:38:03 +0700

    feat: FastPriceFeed deployment script

---
+++ deploy/core/deploy-fast-price-feed.ts
@@ -0,0 +1,53 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers, tenderly, upgrades } from "hardhat";
+import { getImplementationAddress } from "@openzeppelin/upgrades-core";
+import { getConfig, writeConfigFile } from "../utils/config";
+
+const config = getConfig();
+
+const priceDuration = 300;
+const maxPriceUpdateDelay = 3600;
+const minBlockInterval = 0;
+const maxDeviationBasisPoints = 1000;
+const tokenManager = "0x6629eC35c8Aa279BA45Dbfb575c728d3812aE31a";
+const positionRouter = "0x6629eC35c8Aa279BA45Dbfb575c728d3812aE31a";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const deployer = (await ethers.getSigners())[0];
+
+  console.log(`> Deploying FastPriceFeed Contract`);
+  const FastPriceFeed = await ethers.getContractFactory(
+    "FastPriceFeed",
+    deployer
+  );
+  const fastPriceFeed = await upgrades.deployProxy(FastPriceFeed, [
+    priceDuration,
+    maxPriceUpdateDelay,
+    minBlockInterval,
+    maxDeviationBasisPoints,
+    tokenManager,
+    positionRouter,
+  ]);
+  console.log(`> ⛓ Tx submitted: ${fastPriceFeed.deployTransaction.hash}`);
+  await fastPriceFeed.deployTransaction.wait(3);
+  console.log(`> Deployed at: ${fastPriceFeed.address}`);
+
+  config.Pools.PLP.fastPriceFeed = fastPriceFeed.address;
+  writeConfigFile(config);
+
+  const implAddress = await getImplementationAddress(
+    ethers.provider,
+    fastPriceFeed.address
+  );
+
+  console.log(`> Verifying contract on Tenderly...`);
+  await tenderly.verify({
+    address: implAddress,
+    name: "FastPriceFeed",
+  });
+  console.log(`> ✅ Done!`);
+};
+
+export default func;
+func.tags = ["FastPriceFeed"];
