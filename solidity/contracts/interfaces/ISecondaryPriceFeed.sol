865b0b40aaa44c5bbe3ad76494c3596dab1628f2
Jonathan Xina <john.xina.p88@gmail.com>
2022-12-07 11:29:04 +0700

    feat: pool oracle with secondary price feed

---
+++ solidity/contracts/interfaces/ISecondaryPriceFeed.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.17;
+
+interface ISecondaryPriceFeed {
+  function getPrice(
+    address _token,
+    uint256 _referencePrice,
+    bool _maximise
+  ) external view returns (uint256);
+}
