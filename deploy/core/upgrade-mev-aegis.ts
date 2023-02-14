5f47d97ca4b42f32c2742a8da8a087f6b5cbffe0
Jonathan Xina <john.xina.p88@gmail.com>
2023-02-14 18:00:38 +0700

    feat: deployment scritps

--- deploy/core/upgrade-mev-aegis.ts
+++ deploy/core/upgrade-mev-aegis.ts
@@ -3,26 +3,55 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { ethers, upgrades, tenderly } from "hardhat";
 import { getConfig, writeConfigFile } from "../utils/config";
 import { getImplementationAddress } from "@openzeppelin/upgrades-core";
+import { MEVAegis__factory, Ownable__factory } from "../../typechain";
+import { Transaction, queueTransaction } from "../utils/timelock";
+import { writeJson } from "../utils/file";
 
 const config = getConfig();
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const TITLE = "upgrade_mev_aegis";
+  const EXACT_ETA = "1676457805";
+
+  const timelockTransactions: Array<Transaction> = [];
   const deployer = (await ethers.getSigners())[0];
+  const chainId = await deployer.getChainId();
   const MEVAegis = await ethers.getContractFactory("MEVAegis", deployer);
+  const proxyAdmin = Ownable__factory.connect(config.ProxyAdmin, deployer);
   const newMEVAegisImp = await upgrades.prepareUpgrade(
     config.Pools.PLP.mevAegis,
     MEVAegis
   );
 
   console.log(`> New MEVAegis Implementation address: ${newMEVAegisImp}`);
-  const upgradeTx = await upgrades.upgradeProxy(
-    config.Pools.PLP.mevAegis,
-    MEVAegis
-  );
-  console.log(`> ⛓ Tx is submitted: ${upgradeTx.deployTransaction.hash}`);
-  console.log(`> Waiting for tx to be mined...`);
-  await upgradeTx.deployTransaction.wait(3);
-  console.log(`> Tx is mined!`);
+
+  const owner = await proxyAdmin.owner();
+  if (owner === config.Timelock) {
+    timelockTransactions.push(
+      await queueTransaction(
+        chainId,
+        `> Queue tx to upgrade ${config.Pools.PLP.mevAegis}`,
+        config.ProxyAdmin,
+        "0",
+        "upgrade(address,address)",
+        ["address", "address"],
+        [config.Pools.PLP.mevAegis, newMEVAegisImp],
+        EXACT_ETA
+      )
+    );
+
+    const timestamp = Math.floor(Date.now() / 1000);
+    writeJson(`${timestamp}_${TITLE}`, timelockTransactions);
+  } else {
+    const upgradeTx = await upgrades.upgradeProxy(
+      config.Pools.PLP.mevAegis,
+      MEVAegis
+    );
+    console.log(`> ⛓ Tx is submitted: ${upgradeTx.deployTransaction.hash}`);
+    console.log(`> Waiting for tx to be mined...`);
+    await upgradeTx.deployTransaction.wait(3);
+    console.log(`> Tx is mined!`);
+  }
 
   await tenderly.verify({
     address: newMEVAegisImp.toString(),
